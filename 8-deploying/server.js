f0abd201802f4a930a6aadf7ae907180f0b8756e
Cliff <cliff.crerar@gmail.com>
2019-05-19 22:42:19 +0200

    new shit

--- 8-deploying/server.js
+++ 8-deploying/server.js
@@ -1,5 +1,5 @@
-const express = require('./node_modules/express')
-const next = require('./node_modules/next')
+const express = require('express')
+const next = require('next')
 
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
